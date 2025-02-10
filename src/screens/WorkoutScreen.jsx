93240e8ff76c981b37ab3a3a0022023f1417e1bd
Gurnoor Singh Batth <gurnoorbatth10@gmail.com>
2025-02-10 07:39:17 -0800

    Created the Category icons

--- src/screens/WorkoutScreen.jsx
+++ src/screens/WorkoutScreen.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 import Welcome from '../components/welcome'
 import WorkoutOTD from '../components/WorkoutOTD';
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import Separator from '../components/Separator'
+import Category from '../components/Category'
 const WorkoutScreen = () => {
   return (
     <SafeAreaView className='mx-[1%]'>
       <Welcome></Welcome>
       <WorkoutOTD></WorkoutOTD>
+      <Separator />
+      <Category></Category>
     </SafeAreaView>
   )
 }
