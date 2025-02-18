16cf166fe9ce65c1a29467dda2396e80fdc96f10
Gurnoor Singh Batth <gurnoorbatth10@gmail.com>
2025-02-18 11:56:21 -0800

    Styled the exercise instructions page

--- src/screens/ExerciseScreen.jsx
+++ src/screens/ExerciseScreen.jsx
@@ -34,7 +34,7 @@ const ExerciseScreen = () => {
   },[]);
 
   return (
-    <View>
+    <View className='flex-1'>
      {gifUrl ? (
       <Image source={{uri: gifUrl}} className='w-full h-80'/>
     ) :(
@@ -42,47 +42,47 @@ const ExerciseScreen = () => {
       <ActivityIndicator size={"large"} color={"gray"}/>
     </View>)
       }
-      <ScrollView className=''>
-        <View>
-          <Text className=''>{item.title}</Text>
-          <Text className=''>
-            {item.category.split(',').map((cat, index) => (
-              <View key={index}>
-                <View>
-                  <Text>{cat}</Text>
+      <ScrollView >
+        <View className='mt-4 mx-3'>
+          <Text className='text-2xl font-bold text-center mb-1'>{item.title}</Text>
+          <Text className='text-gray-500 mt-1'>
+            {item.category.split(', ').map((cat, index) => (
+              <View key={index} className='mr-2'>
+                <View className='mr-2 bg-gray-300 rounded-2xl px-2 '>
+                  <Text className='text-fuchsia-500'>{cat}</Text>
                 </View>
               </View>
             ))}
           </Text>
-          <View>
-            <Text>Itensity:</Text>
-            <Text>{item.intensity}</Text>
+          <View className='flex-row items-center space-x-2 mt-2'>
+            <Text className='font-semibold text-blue-500'>Itensity:</Text>
+            <Text className='text-cyan-400 italic text-base'>{item.intensity}</Text>
           </View>
-          <Text>Instructions:</Text>
-          <View>
+          <Text className='text-xl font-semibold mt-4'>Instructions:</Text>
+          <View className='mt-2'>
             {item.instructions.map((instruction) => (
-              <View key={instruction.step}>
-                <Text>{instruction.step}</Text>
-                <Text>{instruction.text}</Text>
+              <View key={instruction.step} className='flex-row items-center space-x-2 '>
+                <Text className='text-base text-gray-600 mb-2'>{instruction.step}.</Text>
+                <Text className='ml-2 text-base '>{instruction.text}</Text>
               </View>
             ))}
           </View>
         </View>
-        <View>
-          <TouchableOpacity>
-            <Text>-</Text>
+        <View className='mt-4 flex-row items-center justify-center space-x-3'>
+          <TouchableOpacity className='items-center justify-center w-14 h-14 bg-red-500 rounded-full' >
+            <Text className='text-white text-4xl'>-</Text>
           </TouchableOpacity>
-          <Text>10 Seconds</Text>
-          <TouchableOpacity>
-            <Text>+</Text>
+          <Text className='text-xl font-bold'>10 Seconds</Text>
+          <TouchableOpacity className='items-center justify-center w-14 h-14 bg-green-500 rounded-full'>
+            <Text className='text-white text-3xl'>+</Text>
           </TouchableOpacity>
         </View>
-        <View>
+        <View className='mt-4 flex-row  items-center justify-center space-x-3 mb-10'>
           <TouchableOpacity>
-            <Text>Start</Text>
+            <Text className='text-blue-500 text-xl py-2 border rounded-lg border-blue-500 px-4'>START</Text>
           </TouchableOpacity>
           <TouchableOpacity>
-            <Text>Reset</Text>
+            <Text className='text-bray-500 text-xl py-2 border rounded-lg border-gray-500 px-4'>RESET</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
