4475a578d5c6d7db8d8aa7582a93d684aedb376f
Gurnoor Singh Batth <gurnoorbatth10@gmail.com>
2025-04-01 12:15:51 -0700

    Created the timer logic for timer screen

--- src/screens/TimerScreen.jsx
+++ src/screens/TimerScreen.jsx
@@ -1,10 +1,32 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const TimerScreen = () => {
+
+  const [roundDuration, setRoundDuration] = useState(10);
+  const [restDuration, setRestDuration] = useState(5);
+  const [numberOfRounds, setNumberOfRounds] = useState(2);
+  const [currentRound, setCurrentRound] = useState(1);
+  const [isRunning, setIsRunning] = useState(false);
+  const [isPaused, setIsPaused] = useState(roundDuration);
+  const [isResting, setIsResting] = useState(false);
+  const [resetFlag, setResetFlag] = useState(false);  
+
+  useEffect(() => {
+    setTime(isResting ? restDuration : roundDuration);
+  }, [roundDuration, restDuration, isResting, resetFlag]);
+
   return (
     <View>
-      <Text>TimerScreen</Text>
+      <Text>
+        {isRunning
+          ? isResting
+            ? 'Resting' 
+              : `Round ${currentRound}` 
+            : currentRound===1 && time === roundDuration ? "Start" :
+        currentRound === numberOfRounds && time === 0 ? "Finished" : "Pause"}</Text>
+
+        <Text>{`${Math.floor(time / 60)}:${time % 60<10 ? "0" : ''} ${time % 60}`}</Text>
     </View>
   )
 }
