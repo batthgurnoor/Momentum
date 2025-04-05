d021376ca7d1fc4b2f695692effa20ce36c0a1a1
Noor <noor14598@live.ca>
2025-04-05 14:32:22 -0700

    edit workoutOTD file to include notification functionality

--- src/components/WorkoutOTD.jsx
+++ src/components/WorkoutOTD.jsx
@@ -1,6 +1,7 @@
 import { View, Text, ImageBackground,TouchableOpacity } from 'react-native'
 import React from 'react'
 const otdImage = require('../../assets/images/workoutotd.jpg')
+import { useNotification } from 'NotificationContext'
 
 import {
   useFonts,
@@ -12,26 +13,38 @@ const WorkoutOTD = () => {
   let [fontsLoaded] = useFonts({
     Lato_400Regular,
   });
+
+  const { showSuccess, showInfo } = useNotification();
+  
+  const handleWorkoutSelect = () => {
+    showSuccess('Workout of the day loaded!');
+  };
+
   if (!fontsLoaded) {
     return null;
   }
   return (
-    <TouchableOpacity className="items-center justify-center">
+    <TouchableOpacity 
+      className="items-center justify-center" 
+      onPress={handleWorkoutSelect}
+    >
       <View className="rounded-3xl overflow-hidden h-40 w-[80%]">
         <ImageBackground
-           source ={otdImage}
-            className="flex-1 justify-center items-center"
-            resizeMode="cover">
-              <View>
-                <Text className="text-white/70 text-3xl tracking-tighter" style= {{fontFamily: 'Lato_400Regular'}}>
-                  Workout Of the Day
-                </Text>
-              </View>
+          source={otdImage}
+          className="flex-1 justify-center items-center"
+          resizeMode="cover">
+            <View>
+              <Text 
+                className="text-white/70 text-3xl tracking-tighter" 
+                style={{ fontFamily: 'Lato_400Regular' }}
+              >
+                Workout Of the Day
+              </Text>
+            </View>
         </ImageBackground>
       </View>
     </TouchableOpacity>
   )
 }
-
 
 export default WorkoutOTD;
