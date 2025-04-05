a119ad553124d17a276075a3f1574dc1d0027681
Noor <noor14598@live.ca>
2025-04-05 14:32:38 -0700

    edit index.jsx to include notification functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,54 +10,59 @@ import TimerScreen from "../../src/screens/TimerScreen";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { NotificationProvider } from '/NotificationContext';
+import NotificationsContainer from '/Notification';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
-
 export default function App() {
-  function TabNavigator(){
-    return(
+  function TabNavigator() {
+    return (
       <Tab.Navigator
-      screenOptions={({route})=>({
-        tabBarHideOnKeyboard:true,
-        tabBarIcon:({color,size})=>{
-          let iconName;
-          switch(route.name){
-            case "Workout":
-              iconName="dumbbell";
-              return(<FontAwesome6 name={iconName} size={size} color={color} />);
+        screenOptions={({route}) => ({
+          tabBarHideOnKeyboard: true,
+          tabBarIcon: ({color, size}) => {
+            let iconName;
+            switch(route.name) {
+              case "Workout":
+                iconName = "dumbbell";
+                return (<FontAwesome6 name={iconName} size={size} color={color} />);
               case "Timer":
-              iconName="timer-outline";
-              return(<Ionicons name="timer-outline" size={size} color={color} />);
+                iconName = "timer-outline";
+                return (<Ionicons name="timer-outline" size={size} color={color} />);
               case "Calculation":
-              iconName="calculator-outline";
-              return(<Ionicons name="calculator-outline" size={size} color={color} />);
-          }
-        },
-        tabBarShowLabel:false,
-        headerShown:false,
-        tabBarStyle:{
-          backgroundColor:"black",
-          borderTopRightRadius:20,
-          borderTopLeftRadius:20,
-          paddingVertical:5,
-        },
-        tabBarActiveTintColor:"aqua",
-        tabBarInactiveTintColor:"gray"
-
-        })}>
+                iconName = "calculator-outline";
+                return (<Ionicons name="calculator-outline" size={size} color={color} />);
+            }
+          },
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: "black",
+            borderTopRightRadius: 20,
+            borderTopLeftRadius: 20,
+            paddingVertical: 5,
+          },
+          tabBarActiveTintColor: "aqua",
+          tabBarInactiveTintColor: "gray"
+        })}
+      >
         <Tab.Screen name="Workout" component={WorkoutScreen}></Tab.Screen>
         <Tab.Screen name="Timer" component={TimerScreen}></Tab.Screen>
         <Tab.Screen name="Calculation" component={CalculationScreen}></Tab.Screen>
       </Tab.Navigator>
-    )
+    );
   }
+
   return (
-  
-    <Stack.Navigator screenOptions={{headerShown:false}}>
-      <Stack.Screen name='TabNav' component={TabNavigator}></Stack.Screen>
-    </Stack.Navigator>
-  
+    <NotificationProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name='TabNav' component={TabNavigator}></Stack.Screen>
+        </Stack.Navigator>
+        <NotificationsContainer />
+      </NavigationContainer>
+    </NotificationProvider>
   );
 }
